161bd4d5e7968aeaaa0efde79faeff43081a4263
YunYeoJin <duwls0349@naver.com>
2022-08-19 16:26:07 +0900

    Initial Commit

---
+++ NetFlix_CodeBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetFlix_CodeBase
+//
+//  Created by 윤여진 on 2022/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
